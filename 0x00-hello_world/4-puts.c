6dee5a094cb610d341f220afc17a354867b2c944
“ericwahome” <ewahome68@yahoo.com>
2022-06-02 08:49:56 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 	/**
-	 * This is the preferred style for multi-line
-	 * comments in C source code.
-	 * Please use it consistently.
+	 * main is where the execution begins
+	 * 
+	 * 
 	 *
-	 * Description:  A column of asterisks on the left side,
-	 * with beginning and ending almost-blank lines.
+	 * Retun is o when the program executes
+	 *
 	 */
 
 int main(void)
