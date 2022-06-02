150982d7062410796dc2dd2781609ea14f9f1fa8
“ericwahome” <ewahome68@yahoo.com>
2022-06-02 08:54:34 -0700

    44

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 
-	/**
-	 * main is where the execution begins
-	 * 
-	 * 
-	 *
-	 * Retun is o when the program executes
-	 *
-	 */
-
+/**
+* * main is where the execution begins
+*
+* Retun is o when the program executes
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
