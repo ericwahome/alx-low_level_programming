d56997665e20a13cdddb31cbd6243f185498e293
“ericwahome” <ewahome68@yahoo.com>
2022-06-02 10:05:42 -0700

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -This is the  Execution starting point
+ *
+ * Return: Always 1 when the running suceeds
+ */
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 60);
+	return (1);
+}
+
