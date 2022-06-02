4c19c02d36f24deba67bb184678702c70ca852c6
“ericwahome” <ewahome68@yahoo.com>
2022-06-02 09:07:31 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -This is the  Execution starting point
+ *
+ * Return: Always o when the running suceeds
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
