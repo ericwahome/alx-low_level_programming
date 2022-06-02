ea1baee15103c619b6ac81992a16e8afb3747e46
“ericwahome” <ewahome68@yahoo.com>
2022-06-02 10:07:45 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,11 @@
  *
  * Return: Always 1 when the running suceeds
  */
+
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 60);
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
 
