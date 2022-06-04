9640333ad645ba1544441210d82984e2bde9cb1e
“ericwahome” <ewahome68@yahoo.com>
2022-06-04 03:47:39 -0700

    Lower

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -This is the  Execution starting point
+ *
+ * Return: Always o when the running suceeds
+ */
+int main(void)
+{
+	char lowcase;
+
+	for (lowcase = 'a'; low <= 'z'; low++)
+		putchar(lowcase);
+		printf("\n");
+
+	return (0);
+}
