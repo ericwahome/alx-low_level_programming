a90d24a5b9aefba67fcef4148b91c932c655bd27
“ericwahome” <ewahome68@yahoo.com>
2022-06-04 03:34:05 -0700

    pon

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,17 +1,16 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdlib.h>
-
 /**
  * main -This is the  Execution starting point
  *
  * Return: Always o when the running suceeds
  */
-
 int main(void)
 {
-     int n;
-    srand(time(0));
+	int n;
+
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
