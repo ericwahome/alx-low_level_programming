069ee4d1b875bec6d8a8709449894a313c623271
“ericwahome” <ewahome68@yahoo.com>
2022-06-04 04:28:52 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main-Starts here
+ *
+ *Return:The return value is 0 every time it runs
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'z'; lc >= 'a'; lc--)
+		putchar(lc);
+		putchar('\n');
+
+	return (0);
+}
