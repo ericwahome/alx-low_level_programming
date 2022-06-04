6d44bcd21be87979aa5f86e90f6a713276f67066
“ericwahome” <ewahome68@yahoo.com>
2022-06-04 04:25:48 -0700

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main-Starts here
+ *
+ *Return:The return value is 0 every time it runs
+ */
+int main(void)
+{
+	int m;
+
+for (m = '0'; m <= '9'; m++)
+putchar(m);
+putchar('\n');
+
+	return (0);
+}
