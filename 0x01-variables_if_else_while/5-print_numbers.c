e41951f9c094a987baac876026593eea78c6ac91
“ericwahome” <ewahome68@yahoo.com>
2022-06-04 04:16:00 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main-The program starts from here 
+ *
+ *Return:The return value is 0 every time it runs
+ */
+int main(void)
+{
+	int m;
+
+	for (m = 0; m < 10; m++)
+		printf("%d", m);
+		printf("\n");
+
+	return (0);
+}
