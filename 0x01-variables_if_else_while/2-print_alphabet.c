1f398c3eaa003dff00479abf8afcde7f8dbe4f70
“ericwahome” <ewahome68@yahoo.com>
2022-06-04 23:58:33 -0700

    How to be up to date in git

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,16 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /* 
 *Main:Starts executing
-*
 *Return-The retun value is zero
  */
 int main(void)
-	{
-    char lc;
-
-    for(lc='a'; lc<='z'; lc++)
-    {
-        putchar(lc);
-		putchar('\n');
-    }
-
-    return 0;
+{
+char lc;
+for (lc='a'; lc<='z'; lc++)
+{
+putchar(lc)
+}
+return 0;
 }
