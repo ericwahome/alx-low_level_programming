c7bf49c8f1273813e98822e4d1108fe538547971
“ericwahome” <ewahome68@yahoo.com>
2022-06-05 07:26:12 -0700

    2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,14 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* 
-*Main:Starts executing
-*Return-The retun value is zero
+/** 
+*Main:Starts here
+*Return-retuns 0 after running
  */
 int main(void)
 {
 char lc;
-for (lc='a'; lc<='z'; lc++)
+for (lc = 'a'; lc <= 'z'; lc++)
 {
 putchar(lc);
 }
