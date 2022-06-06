205f18bff7a8cee4e75278b5c1afec0d800fb715
“ericwahome” <ewahome68@yahoo.com>
2022-06-06 00:52:56 -0700

    100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,8 +11,6 @@ int main(void)
 int m;
 for (m = 0; m < 90; m++)
  {		
-for ()
- {
 putchar((m / 10) + '0');
 putchar((m % 10) + '0');
  }
@@ -21,7 +19,6 @@ if (m != 89)
 putchar(',');
 putchar(' ');
  }	
- }
 putchar('\n');
 return (0);
  }
