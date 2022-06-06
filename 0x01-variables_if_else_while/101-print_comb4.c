b317080275f0043da523d2a76e5d2075e81f55ae
“ericwahome” <ewahome68@yahoo.com>
2022-06-06 01:04:06 -0700

    101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Starts here
+ *
+ * Return: is 0 on success
+ */
+ int main(void)
+{
+	int m, r, q;
+
+	for (m = '0'; m < '9'; m++)
+	{
+		for (r = m + 1; r <= '9'; r++)
+		{
+			for (q = r + 1; q <= '9'; q++)
+			{
+				if ((r != m) != q)
+				{
+					putchar(m);
+					putchar(r);
+					putchar(q);
+
+					if (m == '7' && r == '8')
+						continue;
+
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
