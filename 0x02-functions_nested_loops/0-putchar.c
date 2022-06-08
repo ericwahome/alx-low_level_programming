2978bdf04147ab832c12155422d51e2c6c7c8741
“ericwahome” <ewahome68@yahoo.com>
2022-06-08 05:40:40 -0700

    1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - starts here
+ *
+ * Return: is 0 on success
+ */
+int main(void)
+{
+	char pc = "_putchar";
+
+	while (pc)
+	{
+		_putchar(pc);
+		pc++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
