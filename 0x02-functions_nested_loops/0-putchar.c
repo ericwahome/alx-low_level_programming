eb65862f8b14d24a36fc9ff1669fcf9c4351eb5e
“ericwahome” <ewahome68@yahoo.com>
2022-06-08 06:49:33 -0700

    1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,17 @@
 #include "main.h"
+
 /**
- * main - starts here
- *
- * Return: is 0 on success
+ * main - Starts here
+ * prints the word _putchar, followed by a new line.
+ * Return: 0.
  */
 int main(void)
 {
-	char pc = "_putchar";
+	char wrd[8] = "_putchar";
+	int m;
 
-	while (pc)
-	{
-		_putchar(pc);
-		pc++;
-	}
+	for (m = 0; m < 8; m++)
+		_putchar(wrd[m]);
 	_putchar('\n');
 
 	return (0);
