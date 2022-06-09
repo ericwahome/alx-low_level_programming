3259c4507510f304a0e3f8b3e6ab9306e456088e
“ericwahome” <ewahome68@yahoo.com>
2022-06-09 07:32:07 -0700

    00

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -11,6 +11,11 @@ int main(void)
 
         i = 98;
         positive_or_negative(i);
-
+		if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
+	else
+		printf("%d is negative\n", i);
         return (0);
 }
