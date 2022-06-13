b750fefa1a955c128b0e87d9dd837803e9916a88
“ericwahome” <ewahome68@yahoo.com>
2022-06-13 07:01:43 -0700

    Arrays

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * void reset_to_98 - resets the value to 98
+ *
+ * Return: Always 0.
+ */
+void reset_to_98(int *n);
+{
+    int n;
+    n = 402;
+	int *p = &n;
+    _putchar(n);
+	_putchar ("\n"
+    *p = 98;
+    _putchar(n);
+    return (0);
+}
