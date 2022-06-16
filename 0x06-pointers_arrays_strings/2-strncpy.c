39e9d2c8205b8976da840303854157a873ebc053
“ericwahome” <ewahome68@yahoo.com>
2022-06-16 03:03:22 -0700

    How to be up to date in git

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,27 +1,21 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _strncpy - cpoies n bytes of src to the dest string
- * @dest: string to copy to
- * @src: string being copied
- * @n: largest number of bytes to copy
- *
- * Return: address of dest
+ * _strncpy - Entry point
+ * @dest: copy to
+ * @src: copy from
+ * @n: input number of char
+ * Return: Always 0 (Success)
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int a;
 
-	a = 0;
-	while (a < n && *(src + a))
-	{
-		*(dest + a) = *(src + a);
-		a++;
-	}
-	while (a < n)
-	{
-		*(dest + a) = '\0';
-		i++;
-	}
+	for (a = 0; src[a] != '\0'; a++)
+		if (a < n)
+			dest[a] = src[a];
+		while (a < n)
+			dest[a++] = '\0';
+
 	return (dest);
 }
