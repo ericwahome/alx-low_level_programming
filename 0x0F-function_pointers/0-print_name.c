5da71ba8eb1f782444dd448f2f4b3db101a5acf0
“ericwahome” <ewahome68@yahoo.com>
2022-07-07 05:36:20 -0700

    Pointas

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,13 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "function_pointers.h"
-
 /**
- * print_name - a function that prints a name.
- * @name: pointer to name to print
- * @f: function to print name
- * Return: Nothing
+ * print_name - f prints a name
+ * @name: name
+ * @f: pointer to print upp or non upp func
+ * Return: void
  */
-void print_name(char *name, void (*f)(char *name))
+void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
