c38171ed6e64ac118c869a33dbd38a58e4a29544
“ericwahome” <ewahome68@yahoo.com>
2022-07-07 05:43:21 -0700

    Point

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,15 @@
+#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name
- * @name: name to print
- * @f: function to print with
- *
- * Return: void
+ * print_name - prints a name.
+ * @name: pointer to name to print
+ * @f: function to print name
+ * Return: zero on success
  */
-void print_name(char *name, void (*f)(char *))
+void print_name(char *name, void (*f)(char *name))
 {
-	if (name != NULL && f != NULL)
-		f(name);
+if (name == NULL || f == NULL)
+return;
+f(name);
 }
