1cd34e9ed96a68d4e146945dc7abb31504765b76
“ericwahome” <ewahome68@yahoo.com>
2022-07-07 05:32:41 -0700

    Pointas

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,10 +2,10 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name
+ * print_name - a function that prints a name.
  * @name: pointer to name to print
  * @f: function to print name
- * Return: zero on success
+ * Return: Nothing
  */
 void print_name(char *name, void (*f)(char *name))
 {
