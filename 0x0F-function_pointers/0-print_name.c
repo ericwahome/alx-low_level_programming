ccdaf45e2b008bb0e1e9aec966a13406da3d5a22
“ericwahome” <ewahome68@yahoo.com>
2022-07-07 05:39:38 -0700

    Pointed

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,14 @@
 #include "function_pointers.h"
+
 /**
- * print_name - f prints a name
- * @name: name
- * @f: pointer to print upp or non upp func
+ * print_name - prints a name
+ * @name: name to print
+ * @f: function to print with
+ *
  * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
-	f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
