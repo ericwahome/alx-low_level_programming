bbea715ee7c3e3de6f74910c7b239564da263bd4
“ericwahome” <ewahome68@yahoo.com>
2022-07-07 10:14:21 -0700

    Variadic

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,53 +1,50 @@
-#include "variadic_functions.h"
 #include <stdarg.h>
-#include <stdlib.h>
 #include <stdio.h>
-
+#include "variadic_functions.h"
 /**
- * print_all - Prints anyting.
- * @format: A list of type of arguments passed to the function.
- * Return: Nothing
+ * print_all - Entry Point
+ * c = char, i = int, f = float, s = char * (if null print (nil))
+ * @format: list of arg types
+ * Return: 0
  */
 void print_all(const char * const format, ...)
 {
-	va_list ap;
-	char *temp;
-	int m = 0;
+	va_list valist;
+	int n = 0, i = 0;
+	char *sep = ", ";
+	char *str;
 
-	va_start(ap, format);
-	while (format == NULL)
+	va_start(valist, format);
+
+	while (format && format[i])
+		i++;
+
+	while (format && format[n])
 	{
-		printf("\n");
-		return;
-	}
-	while (format[m])
-	{
-		switch (format[m])
+		if (n  == (i - 1))
 		{
-			case 'c':
-				printf("%c", (char) va_arg(ap, int));
-				break;
-			case 'm':
-				printf("%d", va_arg(ap, int));
-				break;
-			case 'f':
-				printf("%f", (float) va_arg(ap, double));
-				break;
-			case 's':
-				temp = va_arg(ap, char*);
-				if (temp != NULL)
-				{
-					printf("%s", temp);
-					break;
-				}
-				printf("(nil)");
-				break;
+			sep = "";
 		}
-		if ((format[m] == 'c' || format[i] == 'm' || format[m] == 'f' ||
-					format[m] == 's') && format[(m + 1)] != '\0')
-			printf(", ");
-		m++;
+		switch (format[n])
+		{
+		case 'c':
+			printf("%c%s", va_arg(valist, int), sep);
+			break;
+		case 'i':
+			printf("%d%s", va_arg(valist, int), sep);
+			break;
+		case 'f':
+			printf("%f%s", va_arg(valist, double), sep);
+			break;
+		case 's':
+			str = va_arg(valist, char *);
+			if (str == NULL)
+				str = "(nil)";
+			printf("%s%s", str, sep);
+			break;
+		}
+		n++;
 	}
-	va_end(ap);
 	printf("\n");
+	va_end(valist);
 }
